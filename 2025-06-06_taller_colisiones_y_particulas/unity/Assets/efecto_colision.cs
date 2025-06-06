813f74efd5ad9b6d720b85f8f62478ae3559fdef
Diego-Alvarez19 <diegolds221@gmail.com>
2025-06-06 13:38:30 -0500

    complete Unity collision/particle effects workshop

---
+++ 2025-06-06_taller_colisiones_y_particulas/unity/Assets/efecto_colision.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class ColisionParticulas : MonoBehaviour
+{
+    public ParticleSystem efecto;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (efecto != null)
+        {
+            efecto.transform.position = collision.contacts[0].point;
+            efecto.Play();
+        }
+    }
+}
+
+
+
+
